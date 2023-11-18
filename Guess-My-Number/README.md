1afcf6e0cedcdb70f7b7a8012836a982465ae313
Maged Saif <magedsaif21@gmail.com>
2023-11-18 13:47:54 +0200

    first project

---
+++ Guess-My-Number/README.md
@@ -0,0 +1,21 @@
+/**
+ * README for Guess My Number Project
+ * 
+ * This project is part of "The Complete JavaScript Course 2024: From Zero to Expert!".
+ * It is a game where the user tries to guess a randomly generated number within a given range.
+ * 
+ * How to play:
+ * 1. The user enters a number in the input field.
+ * 2. The program compares the user's guess with the randomly generated number.
+ * 3. If the guess is correct, the user wins. Otherwise, the program provides hints to help the user guess the number.
+ * 4. The user can keep guessing until they either win or reach the maximum number of attempts.
+ * 
+ * This project covers concepts such as:
+ * - DOM manipulation
+ * - Event handling
+ * - Conditional statements
+ * - Random number generation
+ * - Error handling
+ * 
+ * Enjoy playing the game!
+ */
---
+++ Guess-My-Number/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+let secretNumber = Math.trunc(Math.random()*20)+1;
+
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+// addig event listner whenever i click check
+document.querySelector('.check').addEventListener
+('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    // when there is no input
+    if(!guess) {
+        displayMessage('No number');
+    }
+    // when player wins
+    else if (guess === secretNumber) {
+            displayMessage('Correct Number');
+            document.querySelector('.number').textContent = secretNumber
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+    }
+    } // when guess is wrong
+    else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? 'Number is too high': 'Number is too low');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('You lost the game!')
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+// implement a game rest functionality, so that the player can make a new guess.
+
+document.querySelector('.again').addEventListener
+('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1 ;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    displayMessage('Start Guessing...');
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = '';
+});
